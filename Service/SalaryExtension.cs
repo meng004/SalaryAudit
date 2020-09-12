93a9f4c08fd9ee8f1b6cad902fff2f55338e5d9c
mlemon <meng004@gmail.com>
2020-09-12 11:52:50 +0800

    工资类扩展方法，提供应发累计、实发累计

---
+++ Service/SalaryExtension.cs
@@ -0,0 +1,31 @@
+﻿using JournalVoucherAudit.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalVoucherAudit.Service
+{
+    public static class SalaryExtension
+    {
+        /// <summary>
+        /// 应发累计
+        /// </summary>
+        /// <param name="salaries"></param>
+        /// <returns></returns>
+        public static decimal TotalPayable(this IList<Salary> salaries)
+        {
+            return salaries.Sum(t => t.Payable);
+        }
+        /// <summary>
+        /// 实发累计
+        /// </summary>
+        /// <param name="salaries"></param>
+        /// <returns></returns>
+        public static decimal TotalActual(this IList<Salary> salaries)
+        {
+            return salaries.Sum(t => t.Actual);
+        }
+    }
+}

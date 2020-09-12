cb6678c0a3ae303dd012da9b2fbca43f7f684bf4
mlemon <meng004@gmail.com>
2020-09-12 11:54:15 +0800

    增加应发调节后是否平衡的状态

--- Service/SalaryAudit.cs
+++ Service/SalaryAudit.cs
@@ -218,7 +218,7 @@ namespace JournalVoucherAudit.Service
                 //取退休，按部门、用户编号升序
                 var retired = Retired.OrderBy(t => t.DepartmentName)
                                      .ThenBy(t => t.UserId)
-                                     .Select(t => new Balance(new Salary(), t));
+                                     .Select(t => new Balance(t, new Salary()));
 
                 //返回
                 var balancedWithNew = balanced.Concat(news);
@@ -235,22 +235,35 @@ namespace JournalVoucherAudit.Service
         {
             get
             {
-                //按实发降序，按部门、用户ID升序
-                var balanced = BalancesDetailed.OrderByDescending(t => t.Payable)
-                                               .ThenBy(t => t.DepartmentName)
-                                               .ThenBy(t => t.UserId);
-                //因为用户关注在职人员，所以对应记录放在最上面，包括变动 + 新入职
-                //新入职按编号排序
-                var balancedWithNew = balanced.Concat(NewSalaries.OrderBy(t => t.DepartmentName)
-                                                                 .ThenBy(t => t.UserId));
-                // + 退休，按部门排序
-                var withRetired = balancedWithNew.Concat(Retired.OrderBy(t => t.DepartmentName)
-                                                                .ThenBy(t => t.UserId));
-
-                return withRetired.ToList();
+                ////按实发降序，按部门、用户ID升序
+                //var balanced = BalancesDetailed.OrderByDescending(t => t.Payable)
+                //                               .ThenBy(t => t.DepartmentName)
+                //                               .ThenBy(t => t.UserId);
+                ////因为用户关注在职人员，所以对应记录放在最上面，包括变动 + 新入职
+                ////新入职按编号排序
+                //var balancedWithNew = balanced.Concat(NewSalaries.OrderBy(t => t.DepartmentName)
+                //                                                 .ThenBy(t => t.UserId));
+                //// + 退休，按部门排序
+                //var withRetired = balancedWithNew.Concat(Retired.OrderBy(t => t.DepartmentName)
+                //                                                .ThenBy(t => t.UserId));
+                var result = Mashup.Select(t => t.Detail);
+                return result.ToList();
             }
         }
+        /// <summary>
+        /// 应发余额是否平衡
+        /// </summary>
+        public bool IsBalanced
+        {
+            get
+            {
+                //上月应发调节后余额，上月应发 - 上月应发差额小计
+                var last = _last_month_salaries.TotalPayable() - Mashup.BalancePayableOfLast();
+                //本月应发调节后余额，本月应发 - 本月应发差额小计
+                var current = _current_month_salaries.TotalPayable() - Mashup.BalancePayableOfCurrent();
 
-
+                return decimal.Equals(last, current);
+            }
+        }
     }
 }

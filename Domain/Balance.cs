d1268f3b78a8e8b1ee6e098cb7396acba1d78c18
mlemon <meng004@gmail.com>
2020-09-22 21:53:15 +0800

    更改工资差额类为抽象类，差额属性为抽象方法

--- Domain/Balance.cs
+++ Domain/Balance.cs
@@ -7,26 +7,27 @@ using System.Threading.Tasks;
 namespace JournalVoucherAudit.Domain
 {
     /// <summary>
-    /// 差额
+    /// 工资差额
+    /// 抽象类
     /// </summary>
-    public class Balance
+    public abstract class Balance<T> where T: User,new() 
     {
         #region 字段
 
         /// <summary>
         /// 上月工资
         /// </summary>
-        private Salary _last;
+        protected T _last;
         /// <summary>
         /// 本月工资
         /// </summary>
-        private Salary _current;
+        protected T _current;
         /// <summary>
         /// 构造函数
         /// </summary>
         /// <param name="last">上月工资</param>
         /// <param name="current">本月工资</param>
-        public Balance(Salary last, Salary current)
+        public Balance(T last, T current)
         {
             _last = last;
             _current = current;
@@ -41,7 +42,6 @@ namespace JournalVoucherAudit.Domain
         public string DepartmentName => _current.MonthStatus == MonthStatus.Unknown 
                                                              ? _last.DepartmentName 
                                                              : _current.DepartmentName;
-
         /// <summary>
         /// 人员代码
         /// </summary>
@@ -88,11 +88,11 @@ namespace JournalVoucherAudit.Domain
         /// <summary>
         /// 上月工资
         /// </summary>
-        public Salary Last { get { return _last; } }
+        public T Last { get { return _last; } }
         /// <summary>
         /// 本月工资
         /// </summary>
-        public Salary Current { get { return _current; } }
+        public T Current { get { return _current; } }
         /// <summary>
         /// 上月应发
         /// </summary>
@@ -112,51 +112,55 @@ namespace JournalVoucherAudit.Domain
         /// <summary>
         /// 工资差额
         /// </summary>
-        public Salary Detail
-        {
-            get
-            {
-                var salary = new Salary
-                {
-                    UserId = this.UserId,
-                    UserName = this.UserName,
-                    DepartmentName = this.DepartmentName,
-                    //应发
-                    Position = _current.Position - _last.Position,
-                    Scale = _current.Scale - _last.Scale,
-                    Performance = _current.Performance - _last.Performance,
-                    MonthlyReward = _current.MonthlyReward - _last.MonthlyReward,
-                    Talent = _current.Talent - _last.Talent,
-                    Title = _current.Title - _last.Title,
-                    HealthOfFemale = _current.HealthOfFemale - _last.HealthOfFemale,
-                    HousingSubsidy = _current.HousingSubsidy - _last.HousingSubsidy,
-                    TenPercent = _current.TenPercent - _last.TenPercent,
-                    ProtectingEducation = _current.ProtectingEducation - _last.ProtectingEducation,
-                    SpecialSubsidy = _current.SpecialSubsidy - _last.SpecialSubsidy,
-                    DefenseSubsidy = _current.DefenseSubsidy - _last.DefenseSubsidy,
-                    WageOfTemporaryStaff = _current.WageOfTemporaryStaff - _last.WageOfTemporaryStaff,
-                    PerformanceOfTemporaryStaff = _current.PerformanceOfTemporaryStaff - _last.PerformanceOfTemporaryStaff,
-                    Payable = _current.Payable - _last.Payable,
-                    //扣款
-                    Rent = _current.Rent - _last.Rent,
-                    TotalTax = _current.TotalTax - _last.TotalTax,
-                    Fund = _current.Fund - _last.Fund,
-                    MedicalInsurance = _current.MedicalInsurance - _last.MedicalInsurance,
-                    EndowmentInsurance = _current.EndowmentInsurance - _last.EndowmentInsurance,
-                    OccupationalPension = _current.OccupationalPension - _last.OccupationalPension,
-                    Others = _current.Others - _last.Others,
-                    Water = _current.Water - _last.Water,
-                    Actual = _current.Actual - _last.Actual,
-                    PerformanceOfLastMonth = _current.PerformanceOfLastMonth - _last.PerformanceOfLastMonth,
-                    WithholdingTax = _current.WithholdingTax - _last.WithholdingTax,
-                    //状态标志
-                    MonthStatus = this.MonthStatus,
-                    ChangedStatus = this.ChangedStatus
-                };
-                return salary;
-            }
-        }
-        #endregion
+        public abstract T Detail { get; }
 
+        ///// <summary>
+        ///// 工资差额
+        ///// </summary>
+        //public T Detail
+        //{
+        //    get
+        //    {
+        //        var salary = new Salary
+        //        {
+        //            UserId = this.UserId,
+        //            UserName = this.UserName,
+        //            DepartmentName = this.DepartmentName,
+        //            //应发
+        //            Position = _current.Position - _last.Position,
+        //            Scale = _current.Scale - _last.Scale,
+        //            Performance = _current.Performance - _last.Performance,
+        //            MonthlyReward = _current.MonthlyReward - _last.MonthlyReward,
+        //            Talent = _current.Talent - _last.Talent,
+        //            Title = _current.Title - _last.Title,
+        //            HealthOfFemale = _current.HealthOfFemale - _last.HealthOfFemale,
+        //            HousingSubsidy = _current.HousingSubsidy - _last.HousingSubsidy,
+        //            TenPercent = _current.TenPercent - _last.TenPercent,
+        //            ProtectingEducation = _current.ProtectingEducation - _last.ProtectingEducation,
+        //            SpecialSubsidy = _current.SpecialSubsidy - _last.SpecialSubsidy,
+        //            DefenseSubsidy = _current.DefenseSubsidy - _last.DefenseSubsidy,
+        //            WageOfTemporaryStaff = _current.WageOfTemporaryStaff - _last.WageOfTemporaryStaff,
+        //            PerformanceOfTemporaryStaff = _current.PerformanceOfTemporaryStaff - _last.PerformanceOfTemporaryStaff,
+        //            Payable = _current.Payable - _last.Payable,
+        //            //扣款
+        //            Rent = _current.Rent - _last.Rent,
+        //            TotalTax = _current.TotalTax - _last.TotalTax,
+        //            Fund = _current.Fund - _last.Fund,
+        //            MedicalInsurance = _current.MedicalInsurance - _last.MedicalInsurance,
+        //            EndowmentInsurance = _current.EndowmentInsurance - _last.EndowmentInsurance,
+        //            OccupationalPension = _current.OccupationalPension - _last.OccupationalPension,
+        //            Others = _current.Others - _last.Others,
+        //            Water = _current.Water - _last.Water,
+        //            Actual = _current.Actual - _last.Actual,
+        //            PerformanceOfLastMonth = _current.PerformanceOfLastMonth - _last.PerformanceOfLastMonth,
+        //            WithholdingTax = _current.WithholdingTax - _last.WithholdingTax,
+        //            //状态标志
+        //            MonthStatus = this.MonthStatus,
+        //            ChangedStatus = this.ChangedStatus
+        //        };
+        //        return salary;
+        //    }
+        //}
+        #endregion
     }
 }
